a82bda040571983661bf59fe593f725f2587b15c
Ankush <linktoankush@gmail.com>
2020-02-24 00:09:36 +0100

    Initial Code

--- HelloSFML.cpp
+++ HelloSFML.cpp
@@ -1,11 +1,30 @@
 // HelloSFML.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "stdafx.h"
+#include <SFML/Graphics.hpp>
 
+//using namespace sf;
 int main()
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
